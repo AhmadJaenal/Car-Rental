c2a7965f976d3c916b6c1820ed74bb8ef9573fdc
AhmadJaenal <ahmadjaenal629@gmail.com>
2023-12-27 11:44:51 +0700

    [+] Add template dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,24 +3,14 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\PricingController;
 use App\Http\Controllers\ServicesController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-
+// Landing Page
 Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('/about', [AboutController::class, 'about'])->name('about');
 Route::get('/car', [CarController::class, 'car'])->name('car');
@@ -28,3 +18,6 @@ Route::get('/pricing', [PricingController::class, 'pricing'])->name('pricing');
 Route::get('/services', [ServicesController::class, 'services'])->name('services');
 Route::get('/contact', [ContactController::class, 'contact'])->name('contact');
 Route::get('/about', [AboutController::class, 'about'])->name('about');
+
+// Dasboard
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
