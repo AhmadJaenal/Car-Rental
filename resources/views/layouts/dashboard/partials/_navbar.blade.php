68db4b4bf68cfdd9d2202f01965597649f2d7c36
AhmadJaenal <ahmadjaenal629@gmail.com>
2023-12-29 19:15:15 +0700

    [-] Delete some page

--- resources/views/layouts/dashboard/partials/_navbar.blade.php
+++ resources/views/layouts/dashboard/partials/_navbar.blade.php
@@ -82,7 +82,7 @@
                         <i class="ti-settings text-primary"></i>
                         Settings
                     </a>
-                    <a class="dropdown-item">
+                    <a class="dropdown-item" href="{{ route('login') }}">
                         <i class="ti-power-off text-primary"></i>
                         Logout
                     </a>
