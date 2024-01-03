5c996ac3d21e3acad40f5f15915e9146cf1dea98
StefanSetiadi <stefansetiadi452002@gmail.com>
2024-01-03 17:42:06 +0700

    feat [+] update mobil page

--- resources/views/layouts/dashboard/mobil/tambah.blade.php
+++ resources/views/layouts/dashboard/mobil/tambah.blade.php
@@ -38,7 +38,35 @@
 
             <div class="main-panel">
             <div class="content-wrapper">
-                <h1>Tambah Mobil</h1>
+                <div class="row">
+                    <div class="col-12 grid-margin stretch-card">
+                      <div class="card">
+                        <div class="card-body">
+                          <h4 class="card-title">Tambah Data Mobil</h4>
+                          @if($message = Session::get('success'))
+                              <div class="alert alert-success" role="alert">
+                                {{$message}}
+                              </div>
+                          @endif
+                          <form class="forms-sample" action="/tambahdatabarang" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <input type="text" hidden name="id_user" value="">
+                            <div class="form-group">
+                              <label for="nama">Nama</label>
+                              <input type="text" class="form-control" name="nama" id="nama" placeholder="Nama" required autofocus>
+                            </div>
+                            <div class="form-group">
+                              <label for="harga">Harga</label>
+                              <input type="number" class="form-control" name="harga" id="harga" placeholder="harga" required>
+                            </div>
+                  
+                            <button type="submit" class="btn btn-primary mr-2">Kirim</button>
+                            <a class="btn btn-light" href="">Batal</a>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
             </div>
                 <!-- content-wrapper ends -->
                 <!-- partial:partials/_footer.html -->
--- resources/views/layouts/dashboard/mobil/tampil.blade.php
+++ resources/views/layouts/dashboard/mobil/tampil.blade.php
@@ -38,7 +38,95 @@
 
             <div class="main-panel">
             <div class="content-wrapper">
-                <h1>Tampil Mobil</h1>
+                <div class="row">
+                    <div class="col-lg-12 grid-margin stretch-card">
+                      <div class="card">
+                        <div class="card-body">
+                          <h4 class="card-title">Data Mobil</h4>
+                          <form action="/barang" method="GET">
+                          <div class="input-group">
+                            <button type="submit" class="btn btn-outline-secondary rounded-0">
+                                  <i class="icon-search"></i>
+                              </span>
+                            </button>
+                              <input type="text" class="form-control" name="search" id="search" placeholder="Search now" aria-label="search" aria-describedby="search" autofocus>
+                            </div>
+                          </form>
+                            <a href='{{route('tambahmobil')}}' class="btn btn-success mt-2">Tambah</a>
+                          @if($message = Session::get('success'))
+                            <div class="alert alert-success mt-2" role="alert">
+                              {{$message}}
+                            </div>
+                          @endif
+                          <div class="table-responsive pt-3">
+                            <table class="table table-bordered">
+                              <thead>
+                                <th scope="col">No.</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Harga</th>
+                                <th scope="col">Karyawan</th>
+                                <th scope="col">Diperbarui</th>
+                                <th scope="col">Aksi</th>
+                              </thead>
+                              </tbody>
+                              {{-- @php
+                              $no = 1;   
+                              @endphp
+                              @foreach($data as $index => $row)
+                              <tr>
+                                  <th scope="row">{{$index + $data->firstItem()}}</th>
+                                  <td>{{$row->nama}}</td>
+                                  <td>{{$row->harga}}</td>
+                                  <td>{{ucwords($user->where('id_user', $row->id_user)->first()->username)}}</td>
+                                  <td>{{$row->updated_at}}</td>
+                                  <td class="text-center">
+                                      <a href="/editbarang{{$row->id_barang}}" class="btn btn-info">Edit</a>
+                                      <a href="#" id="delete" class="btn btn-danger delete" data-id={{$row->id_barang}} data-nama={{$row->nama}}>Delete</a>
+                                  </td>
+                                </tr>
+                              @php
+                                $no++;
+                              @endphp
+                              @endforeach
+                              @if($no==1) --}}
+                              <tr>
+                                <td colspan="8" class="text-center table-danger">Data Tidak Ditemukan !!!</td>
+                              </tr>
+                              {{-- @endif --}}
+                              </tbody>
+                            </table>
+                            <br>
+                            {{-- {{ $data->links() }} --}}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <script>
+                    $('.delete').click(function(){
+                      var id_barang = $(this).attr("data-id");
+                      var nama = $(this).attr("data-nama");
+                      swal({
+                          title: "Yakin?",
+                          text: "Kamu akan menghapus barang dengan nama : "+nama+"",
+                          icon: "warning",
+                          buttons: true,
+                          dangerMode: true,
+                        })
+                        .then((willDelete) => {
+                          if (willDelete) {
+                            window.location = "/hapusbarang"+id_barang+""
+                          } else {
+                            swal("Data tidak jadi dihapus");
+                          }
+                        });
+                    });
+                    @if(Session::has('successdelete'))
+                      swal("Data berhasil di hapus", {
+                                icon: "success",
+                              });
+                    @endif
+                  </script>
             </div>
                 <!-- content-wrapper ends -->
                 <!-- partial:partials/_footer.html -->
