65d1b80a9c0bea86ff28a3290482ea579308cd66
StefanSetiadi <stefansetiadi452002@gmail.com>
2024-01-23 11:34:11 +0700

    [+] Update database

--- utils/car_rental_db.sql
+++ utils/car_rental_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 20, 2024 at 02:05 PM
+-- Generation Time: Jan 23, 2024 at 05:33 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,20 +42,6 @@ CREATE TABLE `admins` (
 
 INSERT INTO `admins` (`id_admin`, `email`, `username`, `password`, `created_at`, `updated_at`) VALUES
 (1, 'admin@gmail.com', 'Admin001', '$2y$12$1PHVlIvwhZ.zjvBAS0AtQOsU88HlOfof9zKI3H.Z6gNTdgvt9xTJy', '2024-01-20 04:37:34', '2024-01-20 04:37:34');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `kategoris`
---
-
-CREATE TABLE `kategoris` (
-  `id_kategori` bigint(20) UNSIGNED NOT NULL,
-  `tipe_mobil` varchar(20) NOT NULL,
-  `kapasitas` varchar(20) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -107,21 +93,21 @@ CREATE TABLE `mobils` (
 --
 
 INSERT INTO `mobils` (`id_mobil`, `no_plat`, `merk`, `warna`, `tahun`, `sewa_perjam`, `sewa_perhari`, `gambar`, `status`, `id_kategori`, `created_at`, `updated_at`) VALUES
-(1, 'B 1234 AB', 'ALPHARD 2.5 G A/T', 'putih', 2021, 400000, 2000000, 'ALPHARD 2.5 G AT putih 2021.png', 'aktif', 'MPV', '2024-01-20 04:55:31', '2024-01-20 05:00:44'),
-(2, 'D 5678 CD', 'ALPHARD 2.5 G A/T', 'Gold', 2023, 480000, 2400000, 'ALPHARD 2.5 G AT 2023.jpg', 'aktif', 'MPV', '2024-01-20 05:00:29', '2024-01-20 05:00:51'),
-(3, 'F 9102 EF', 'ALPHARD 2.5 G A/T', 'putih', 2023, 480000, 2400000, 'ALPHARD 2.5 G AT putih 2023.png', 'aktif', 'MPV', '2024-01-20 05:02:18', '2024-01-20 05:02:18'),
-(4, 'G 3456 GH', 'CAMRY 2.5V A/T', 'hitam', 2017, 240000, 1200000, 'CAMRY 2.5V AT hitam 2017.jpg', 'aktif', 'sedan', '2024-01-20 05:04:24', '2024-01-20 05:04:24'),
-(5, 'J 7890 IJ', 'CAMRY Facelift 2.5 V A/T', 'putih', 2020, 300000, 1500000, 'CAMRY Facelift 2.5 V AT putih 2020.jpg', 'aktif', 'sedan', '2024-01-20 05:05:35', '2024-01-20 05:05:36'),
-(6, 'K 1234 KL', 'FORTUNER 2.4 VRZ 4X2 A/T DSL GR SPORT', 'silver', 2021, 240000, 1200000, 'Fortuner-2.4-VRZ-2.7SRZ-with-Optional-TRD-Sportivo-Package-Front-e1507284764193.jpg', 'aktif', 'SUV', '2024-01-20 05:09:03', '2024-01-20 05:09:03'),
-(7, 'M 5678 MN', 'FORTUNER 4x2 2.4 VRZ A/T DSL', 'putih', 2018, 200000, 1000000, 'toyota-all-new-fortuner-vrz-still-depan24.jpg', 'aktif', 'SUV', '2024-01-20 05:10:57', '2024-01-20 05:10:57'),
-(8, 'P 9102 OP', 'INNOVA 2.0 G A/T', 'hitam', 2021, 150000, 600000, 'innova-g-so-tu-dong.jpg', 'aktif', 'MPV', '2024-01-20 05:12:44', '2024-01-20 05:12:45'),
-(9, 'R 3456 RS', 'INNOVA 2.4 V A/T DSL', 'hitam', 2021, 175000, 700000, '58040_2021_Toyota_innova_crysta_1.jpg', 'aktif', 'MPV', '2024-01-20 05:14:22', '2024-01-20 05:14:22'),
-(10, 'T 7890 TU', 'INNOVA VENTURER 2.4 A/T DSL', 'hitam', 2020, 220000, 800000, '1.jpg', 'aktif', 'MPV', '2024-01-20 05:15:39', '2024-01-20 05:15:39'),
-(11, 'U 1234 UV', 'INNOVA ZENIX-G 2.0 HV', 'putih', 2023, 200000, 900000, '01.jpg', 'aktif', 'MPV', '2024-01-20 05:17:02', '2024-01-20 05:17:03'),
-(12, 'W 5678 WX', 'LEXUS LX570 A/T DSL', 'hitam', 2015, 2000000, 10000000, 'Lexus-LX-570-2015-(4).jpg', 'aktif', 'SUV', '2024-01-20 05:19:08', '2024-01-20 05:19:08'),
-(13, 'Y 9102 YZ', 'MERCY C300 AMG A/T', 'hitam', 2018, 500000, 2500000, 'image.png', 'aktif', 'sedan', '2024-01-20 05:20:34', '2024-01-20 05:20:34'),
-(14, 'Z 3456 ZA', 'PAJERO SPORT DAKAR (4x2) A/T DSL', 'putih', 2021, 275000, 1300000, '275603-pajero-sport-ready-mitsubishi-pajero-dakar-4x2-at-tahun-2021-warna-putih-img-20220131-wa0012.jpg', 'aktif', 'SUV', '2024-01-20 05:22:20', '2024-01-20 05:22:20'),
-(15, 'X 7890 XY', 'XPANDER Ultimate A/T', 'hitam', 2020, 150000, 600000, 'xpander.jpg', 'aktif', 'MPV', '2024-01-20 05:24:09', '2024-01-20 05:24:09');
+(1, 'B 1234 AB', 'ALPHARD 2.5 G A/T', 'putih', 2021, 400000, 2000000, 'ALPHARD 2.5 G AT putih 2021.png', 'Tersedia', 'MPV', '2024-01-20 04:55:31', '2024-01-22 20:34:58'),
+(2, 'D 5678 CD', 'ALPHARD 2.5 G A/T', 'Gold', 2023, 480000, 2400000, 'ALPHARD 2.5 G AT 2023.jpg', 'Dipesan', 'MPV', '2024-01-20 05:00:29', '2024-01-22 20:40:20'),
+(3, 'F 9102 EF', 'ALPHARD 2.5 G A/T', 'putih', 2023, 480000, 2400000, 'ALPHARD 2.5 G AT putih 2023.png', 'Tersedia', 'MPV', '2024-01-20 05:02:18', '2024-01-20 05:02:18'),
+(4, 'G 3456 GH', 'CAMRY 2.5V A/T', 'hitam', 2017, 240000, 1200000, 'CAMRY 2.5V AT hitam 2017.jpg', 'Tersedia', 'sedan', '2024-01-20 05:04:24', '2024-01-20 05:04:24'),
+(5, 'J 7890 IJ', 'CAMRY Facelift 2.5 V A/T', 'putih', 2020, 300000, 1500000, 'CAMRY Facelift 2.5 V AT putih 2020.jpg', 'Tersedia', 'sedan', '2024-01-20 05:05:35', '2024-01-20 05:05:36'),
+(6, 'K 1234 KL', 'FORTUNER 2.4 VRZ 4X2 A/T DSL GR SPORT', 'silver', 2021, 240000, 1200000, 'Fortuner-2.4-VRZ-2.7SRZ-with-Optional-TRD-Sportivo-Package-Front-e1507284764193.jpg', 'Dipesan', 'SUV', '2024-01-20 05:09:03', '2024-01-22 20:37:24'),
+(7, 'M 5678 MN', 'FORTUNER 4x2 2.4 VRZ A/T DSL', 'putih', 2018, 200000, 1000000, 'toyota-all-new-fortuner-vrz-still-depan24.jpg', 'Tersedia', 'SUV', '2024-01-20 05:10:57', '2024-01-20 05:10:57'),
+(8, 'P 9102 OP', 'INNOVA 2.0 G A/T', 'hitam', 2021, 150000, 600000, 'innova-g-so-tu-dong.jpg', 'Tersedia', 'MPV', '2024-01-20 05:12:44', '2024-01-20 05:12:45'),
+(9, 'R 3456 RS', 'INNOVA 2.4 V A/T DSL', 'hitam', 2021, 175000, 700000, '58040_2021_Toyota_innova_crysta_1.jpg', 'Tersedia', 'MPV', '2024-01-20 05:14:22', '2024-01-20 05:14:22'),
+(10, 'T 7890 TU', 'INNOVA VENTURER 2.4 A/T DSL', 'hitam', 2020, 220000, 800000, '1.jpg', 'Tersedia', 'MPV', '2024-01-20 05:15:39', '2024-01-20 05:15:39'),
+(11, 'U 1234 UV', 'INNOVA ZENIX-G 2.0 HV', 'putih', 2023, 200000, 900000, '01.jpg', 'Tersedia', 'MPV', '2024-01-20 05:17:02', '2024-01-20 05:17:03'),
+(12, 'W 5678 WX', 'LEXUS LX570 A/T DSL', 'hitam', 2015, 2000000, 10000000, 'Lexus-LX-570-2015-(4).jpg', 'Tersedia', 'SUV', '2024-01-20 05:19:08', '2024-01-20 05:19:08'),
+(13, 'Y 9102 YZ', 'MERCY C300 AMG A/T', 'hitam', 2018, 500000, 2500000, 'image.png', 'Tersedia', 'sedan', '2024-01-20 05:20:34', '2024-01-20 05:20:34'),
+(14, 'Z 3456 ZA', 'PAJERO SPORT DAKAR (4x2) A/T DSL', 'putih', 2021, 275000, 1300000, '275603-pajero-sport-ready-mitsubishi-pajero-dakar-4x2-at-tahun-2021-warna-putih-img-20220131-wa0012.jpg', 'Tersedia', 'SUV', '2024-01-20 05:22:20', '2024-01-20 05:22:20'),
+(15, 'X 7890 XY', 'XPANDER Ultimate A/T', 'hitam', 2020, 150000, 600000, 'xpander.jpg', 'Tersedia', 'MPV', '2024-01-20 05:24:09', '2024-01-22 21:33:24');
 
 -- --------------------------------------------------------
 
@@ -160,13 +146,23 @@ CREATE TABLE `transactions` (
   `denda` int(11) DEFAULT NULL,
   `status_pembayaran` varchar(255) NOT NULL,
   `status_sewa` varchar(255) DEFAULT NULL,
-  `status_pengembalian` tinyint(1) DEFAULT NULL,
-  `id_mobil` varchar(255) NOT NULL,
-  `id_user` varchar(255) NOT NULL,
-  `id_admin` varchar(255) DEFAULT NULL,
+  `status_pengembalian` varchar(255) DEFAULT NULL,
+  `id_mobil` bigint(20) UNSIGNED NOT NULL,
+  `id_user` bigint(20) UNSIGNED NOT NULL,
+  `id_admin` bigint(20) UNSIGNED NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `transactions`
+--
+
+INSERT INTO `transactions` (`id_transaksi`, `tgl_rental`, `tgl_kembali`, `tgl_pengembalian`, `jam_mulai`, `jam_selesai`, `biaya_sewa`, `total`, `denda`, `status_pembayaran`, `status_sewa`, `status_pengembalian`, `id_mobil`, `id_user`, `id_admin`, `created_at`, `updated_at`) VALUES
+(8, '2024-01-24', '2024-01-24', NULL, '10:00', '12:00', 400000, 48000000, NULL, 'Lunas', 'Diterima', 'Sudah', 1, 2, 1, '2024-01-22 20:31:31', '2024-01-22 20:34:57'),
+(9, '2024-01-24', '2024-01-26', NULL, '12:00', '12:00', 240000, 480000, NULL, 'Tidak', 'Diproses', 'Belum diambil', 6, 2, 1, '2024-01-22 20:37:24', '2024-01-22 20:37:24'),
+(10, '2024-01-23', '2024-01-24', NULL, '11:00', '11:00', 480000, 480000, NULL, 'Tidak', 'Diproses', 'Belum diambil', 2, 3, 1, '2024-01-22 20:40:20', '2024-01-22 20:40:20'),
+(11, '2024-01-23', '2024-01-24', NULL, '10:00', '10:00', 150000, 150000, NULL, 'Lunas', 'Diterima', 'Sudah', 15, 3, 1, '2024-01-22 21:32:44', '2024-01-22 21:33:30');
 
 -- --------------------------------------------------------
 
@@ -198,7 +194,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id_peminjam`, `nik`, `username`, `password`, `email`, `no_hp`, `foto_ktp`, `foto_diri`, `provinsi`, `kota`, `jalan`, `verifikasi`, `remember_token`, `created_at`, `updated_at`) VALUES
 (1, NULL, 'Ahmad Jaenal', '$2y$12$nmI3f4sO1fqHMcRBJsqfoOw0DcXSZvX65ithfGbCE0milUZ110nVy', 'ahmadjaenal@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, 0, 'eEPW5HEI1d', '2024-01-20 04:37:32', '2024-01-20 04:37:32'),
-(2, '3278020405020004', 'Stefan', '$2y$12$2Kze3rji92Vrujjn5uahsO1dtZcS7.QnNFTrItrIKN8as24VmMwNe', 'stefan@gmail.com', '081234567890', 'ktp.png', 'stefan.jpg', 'Jawa Barat', 'Bandung', 'Taman Kopo Indah 1', 0, 'z1qfHAxa4z66SnyNE660j6GV7KNRxFa8dDIIAyPeUam4KDqUcPHcx19X5cu7', '2024-01-20 04:37:33', '2024-01-20 05:44:59'),
+(2, '3278020405020004', 'Stefan', '$2y$12$2Kze3rji92Vrujjn5uahsO1dtZcS7.QnNFTrItrIKN8as24VmMwNe', 'stefan@gmail.com', '081234567890', 'ktp.png', 'stefan.jpg', 'Jawa Barat', 'Bandung', 'Taman Kopo Indah 1', 0, 'kzs75VPoNBr1pO6G3w940he3uL6GnPX6dX9HZIvyPRMSB73Y8SOOihYzub71', '2024-01-20 04:37:33', '2024-01-22 20:39:06'),
 (3, '3278020405020005', 'Albert', '$2y$12$ur9bk2m9v/ZhsAHlJRq4f.lBf6bdaqyLZsSD7ddPypx9/f5zg3apy', 'albert@gmail.com', '085678901234', 'ktp.png', 'albert.jpg', 'Jawa Barat', 'Bandung', 'Neglasari', 0, NULL, '2024-01-20 05:34:18', '2024-01-20 05:48:02'),
 (4, '3278020405020006', 'Belinda', '$2y$12$nx1jC7WLIf0oKrYw2lv5duqohEyLyz/VxBTCELB5MNWulcbCwe5V2', 'belinda@gmail.com', '087765432109', 'ktp.png', 'belinda.jpg', 'Jawa Barat', 'Bandung', 'Ayudia', 0, NULL, '2024-01-20 05:34:38', '2024-01-20 05:55:10'),
 (5, '3278020405020009', 'Christopher', '$2y$12$Q9SF60dJlP.B0z37g.ZVY.fPJccNQI0pPOBBZjihdDqwekbQ/8nXW', 'christopher@gmail.com', '081398765432', 'ktp.png', 'christopher.jpg', 'Jawa Barat', 'Bandung', 'Karangarum', 0, NULL, '2024-01-20 05:34:59', '2024-01-20 05:56:44'),
@@ -220,12 +216,6 @@ INSERT INTO `users` (`id_peminjam`, `nik`, `username`, `password`, `email`, `no_
 ALTER TABLE `admins`
   ADD PRIMARY KEY (`id_admin`),
   ADD UNIQUE KEY `admins_email_unique` (`email`);
-
---
--- Indexes for table `kategoris`
---
-ALTER TABLE `kategoris`
-  ADD PRIMARY KEY (`id_kategori`);
 
 --
 -- Indexes for table `migrations`
@@ -251,7 +241,10 @@ ALTER TABLE `personal_access_tokens`
 -- Indexes for table `transactions`
 --
 ALTER TABLE `transactions`
-  ADD PRIMARY KEY (`id_transaksi`);
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_mobil` (`id_mobil`),
+  ADD KEY `id_admin` (`id_admin`);
 
 --
 -- Indexes for table `users`
@@ -269,12 +262,6 @@ ALTER TABLE `users`
 --
 ALTER TABLE `admins`
   MODIFY `id_admin` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `kategoris`
---
-ALTER TABLE `kategoris`
-  MODIFY `id_kategori` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -298,13 +285,25 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id_transaksi` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id_peminjam` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `transactions`
+--
+ALTER TABLE `transactions`
+  ADD CONSTRAINT `id_admin` FOREIGN KEY (`id_admin`) REFERENCES `admins` (`id_admin`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_mobil` FOREIGN KEY (`id_mobil`) REFERENCES `mobils` (`id_mobil`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `id_user` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_peminjam`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
